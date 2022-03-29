18dd69034e2608ecf6c955fd073289c7829f5c30
David Gordon <moderndsnsam@gmail.com>
2022-03-28 22:00:49 -0400

    Usermodel

--- FlashTyper-MVC/Models/UserModel.cs
+++ FlashTyper-MVC/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,20 @@ namespace FlashTyper_MVC.Models
 {
     public class UserModel
     {
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Please provide a valid Username")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 20 characters.")]
+        public string Username { get; set; }
 
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Please provide a valid password")]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "Password must be between 10 and 100 characters.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
     }
 }

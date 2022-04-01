dd428ac6900b55ce63b9f912b00e69641661606a
David Gordon <moderndsnsam@gmail.com>
2022-03-31 20:25:33 -0400

    Create Account controller

---
+++ FlashTyper-MVC/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using FlashTyper_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using FlashTyperLibrary.Logic;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace FlashTyper_MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Account(UserModel user)
+        {
+            UserLogic.AddUser(user.Username, user.Password);
+            _logger.LogInformation($"New user '{user.Username}' created at {DateTime.Now}");
+
+            return View("Index");
+        }
+    }
+}

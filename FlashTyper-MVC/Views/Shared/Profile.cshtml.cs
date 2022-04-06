209fe15cc8463609332f63b30285ae71a6b1e172
David Gordon <moderndsnsam@gmail.com>
2022-04-05 21:46:42 -0400

    Created a profile page

---
+++ FlashTyper-MVC/Views/Shared/Profile.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace FlashTyper_WEB.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public ProfileModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}

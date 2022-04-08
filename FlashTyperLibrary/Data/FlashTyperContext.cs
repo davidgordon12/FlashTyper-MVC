92976f1c8ce1d1f6146da5ca02d12e1cd901b040
David Gordon <44374315+tehtko@users.noreply.github.com>
2022-04-07 20:35:11 -0400

    Update FlashTyperContext.cs

--- FlashTyperLibrary/Data/FlashTyperContext.cs
+++ FlashTyperLibrary/Data/FlashTyperContext.cs
@@ -1,1 +1,18 @@
 
+using System.Data.SqlClient;
+
+namespace FlashTyperLibrary.Data
+{
+    public class FlashTyperContext
+    {
+        public SqlConnection Cnn { get; set; }
+        public string CnnString { get; set; }
+
+        public FlashTyperContext()
+        {
+            CnnString = "YOUR STRING HERE";
+
+            Cnn = new SqlConnection(CnnString);
+        }
+    }
+}

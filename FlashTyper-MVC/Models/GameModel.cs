2bf50e317edf0729428a3dd4ae15e82115fdc6d2
tehtko <moderndsnsam@gmail.com>
2022-04-15 14:46:24 -0700

    remove usings

--- FlashTyper-MVC/Models/GameModel.cs
+++ FlashTyper-MVC/Models/GameModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FlashTyper_MVC.Models
+﻿namespace FlashTyper_MVC.Models
 {
     public class GameModel
     {
